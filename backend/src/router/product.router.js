a1c31b13ddb5196a82a584dc8e9e00443db84123
RamdlanFaqih <ramfaqih@gmail.com>
2023-12-18 10:12:16 +0700

    commit

--- backend/src/router/product.router.js
+++ backend/src/router/product.router.js
@@ -1,10 +1,12 @@
 import express from "express";
 import productController from "../controller/product.controller.js";
 import upload from "../middleware/upload.js";
+import { requireAuth } from "../middleware/auth.js";
+import verifyToken from "../middleware/staticAuth.js";
 
 const router = express.Router();
 
-router.get("/list-products", productController.list);
+router.get("/list-products", verifyToken, requireAuth, productController.list);
 router.get("/product/:id", productController.getProduct);
 router.post("/insert/product", upload, productController.createProduct);
 
